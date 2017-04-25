3e10931f3c6ea5f3c438a9ec1bb0996b8b71840e
Donald Hyppolite <donald.hyppolite@gmail.com>
2017-04-24 23:37:14 -0600

    Initial Checkin

---
+++ server.R
@@ -0,0 +1,33 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+  output$meanWeightText <- reactive({
+        m <- mean(women[women$height >= input$minheight,]$weight)
+        paste("Average weight for women with height >= ", input$minheight, ":", m)
+    })
+    
+  output$womenPlot <- renderPlot({
+    
+    # generate bins based on input$bins from ui.R
+    
+    # draw the histogram with the specified number of bins
+    plot(weight ~ height, data=women[women$height >= input$minheight,],xlab="height (in)", ylab="weight (lbs)", main="Average Heights and Weights for American Women")
+    model <- lm(weight ~ height, data=women)
+    abline(model)
+    
+  })
+  
+}
+
+
+)
---
+++ ui.R
@@ -0,0 +1,38 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Women Height and Weight Data"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+       helpText("Directions: Use the slider on the left to recalculate the average weights on the right"),
+       sliderInput("minheight",
+                   "Min Height:",
+                   min = 58,
+                   max = 72,
+                   value = 58)
+       
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+       plotOutput("womenPlot"),
+       textOutput("meanWeightText")
+    )
+    
+  )
+))
